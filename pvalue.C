97300f5be14256076301a7f72c2e4a7fbf534379
Daniel Whiteson <daniel@uci.edu>
2014-12-08 15:07:37 -0800

    p value

---
+++ pvalue.C
@@ -0,0 +1,9 @@
+
+
+#include "StandardFrequentistDiscovery.C"
+
+void pvalue()
+{
+  StandardFrequentistDiscovery("monoh_withsm_SRCR_bg11.7_bgslop-0.0_nsig0.0.root","wspace","ModelConfig","data",10000);
+
+}
