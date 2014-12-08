af1f2e3990865d448c9961c2f1b3d4a68cb12acb
Kyle Cranmer <kyle.cranmer@nyu.edu>
2014-12-08 12:23:14 -0800

    clash with merging with chase

--- test_limits.C
+++ test_limits.C
@@ -1,9 +1,12 @@
 // to run from command line:
 // $ root -q "test_limits.C();"
 
+#define LUMI 20.2769
+
 void test_limits() {
 	// note that RooDCB has to be loaded first!!
 	gROOT->ProcessLine(".L RooDCB.C+");
 	gROOT->ProcessLine(".L fit_withsm.C+");
-	test();
+	//limit_bands(11.67,-1.0/60.176,0,1.069/LUMI,0.07,0.05);
+	limit_bands(11.67,-1.0/60.176,0,1.069/LUMI,0.042,0.029);
 }
